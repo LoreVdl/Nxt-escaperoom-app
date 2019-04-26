aba58622d676d872db3e74b5a6e33a8936e71902
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2019-04-26 14:52:44 +0200

    add firebase database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyApuqxYgWTKR8tT-I9mplbOYbLLhwSUKoc",
+    authDomain: "nxt-escape.firebaseapp.com",
+    databaseURL: "https://nxt-escape.firebaseio.com",
+    projectId: "nxt-escape",
+    storageBucket: "nxt-escape.appspot.com",
+    messagingSenderId: "42854546097"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
