1fe57309e5d31335e68618cc1380090ae9e3cbcd
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2019-05-09 12:03:38 +0200

    show words in music page on random places

--- src/app/words-page/words-page.component.ts
+++ src/app/words-page/words-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-words-page',
@@ -7,17 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WordsPageComponent implements OnInit {
   private words: string[] = ["bal", "aap", "fiets", "test"]; 
-  private word; 
+  private word = "bal"; 
   public intervalVar; 
   public intervalVar2; 
   public startWordPosition = 0; 
+  public fullWidth = 400; 
+  public fullHeight = 400; 
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     this.intervalVar = setInterval(function() {
       this.word = this.words[this.startWordPosition]; 
+      var element = document.getElementById("words"); 
+      var elem = document.createElement("div");
+      elem.textContent = this.word;
+      elem.style.position = "absolute";
+      elem.style.left = Math.round(Math.random() * this.fullWidth) + "px";
+      elem.style.top = Math.round(Math.random() * this.fullHeight) + "px";
+      element.appendChild(elem);
+
       this.intervalVar2 = setTimeout(function() {
+        elem.remove();
         if (this.startWordPosition === ((this.words.length)-1)) {
           this.startWordPosition = 0; 
         }
@@ -26,5 +37,9 @@ export class WordsPageComponent implements OnInit {
         }
       }.bind(this), 500)
     }.bind(this), 500)
+  }
+
+  ngOnChange() {
+    
   }
 }
